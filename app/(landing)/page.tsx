a261424d4729bdcd5aed9b6918c9fd096a306bc3
Plode-v <plodevanichaka@gmail.com>
2023-08-21 09:45:22 +0700

    Update landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import { useClerk } from '@clerk/nextjs'
 import Link from 'next/link'
 import { redirect } from "next/navigation";
+import Image from 'next/image'
 
 const LandingPage = () => {
     const { session } = useClerk();
@@ -16,19 +17,28 @@ const LandingPage = () => {
 
   return (
     <div>
-        LandingPage (unprotected)
-        <div>
-            <Link href="/sign-in">
-                <Button>
-                    Sign In
-                </Button>
-            </Link>
-            <Link href="/sign-up">
-                <Button>
-                    Register
-                </Button>
-            </Link>
-        </div>
+        <nav className='h-full p-3 bg-white border-black border-2 flex justify-between items-center mx-3 rounded-lg mt-2'>
+            <div className='text-white'>
+                <Image 
+                    src="/images/Logo.png"
+                    height={50}
+                    width={50}
+                    alt='Logo'
+                />
+            </div>
+            <div className='flex gap-3'>
+                <Link href="/sign-in">
+                    <Button>
+                        Sign In
+                    </Button>
+                </Link>
+                <Link href="/sign-up">
+                    <Button>
+                        Register
+                    </Button>
+                </Link>
+            </div>
+        </nav>
     </div>
   )
 }
