f436f8f09a4570d6a079333b11c05c6d6e6c68b3
Plode-v <plodevanichaka@gmail.com>
2023-09-01 12:29:21 +0700

    update stripe

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -23,8 +23,43 @@ export async function GET() {
         });
 
         if (userSubscription && userSubscription.stripeCustomerId) {
-            
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer: userSubscription.stripeCustomerId,
+                return_url: settingsUrl
+            });
+
+            return new NextResponse(JSON.stringify({ url: stripeSession.url }));
         }
+
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: settingsUrl,
+            cancel_url: settingsUrl,
+            payment_method_types: ["card"],
+            mode: "subscription",
+            billing_address_collection: 'auto',
+            customer_email: user!.emailAddresses[0].emailAddress,
+            line_items: [
+                {
+                    price_data: {
+                        currency: "USD",
+                        product_data: {
+                            name: "Omni Pro",
+                            description: "Unlimited AI Generations"
+                        },
+                        unit_amount: 2000,
+                        recurring: {
+                            interval: "month"
+                        }
+                    },
+                    quantity: 1
+                }
+            ],
+            metadata: {
+                userId,
+            }
+        })
+
+        return new NextResponse(JSON.stringify({ url: stripeSession.url }));
 
     } catch (err) {
         console.log("[STRIPE_ERROR]", err);
